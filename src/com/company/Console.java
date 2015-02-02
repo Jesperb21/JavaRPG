5285275d52b195a8c0b5993c2d1cff687c631af0
Daniel Jensen <lynspitti@hotmail.com>
2015-02-02 12:36:55 +0100

    Movement and Console commands
    added Default console methodes..
    and interactions with user
    added main loop and bot movemement.
    + start of user movement.

---
+++ src/com/company/Console.java
@@ -0,0 +1,83 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+/**
+ * Created by DBJ on 02-02-2015.
+ */
+public final class Console {
+
+    /**
+     * Clears console "Not working
+     */
+    public final static void Clear()
+    {
+        try
+        {
+            final String os = System.getProperty("os.name");
+            if (os.contains("Windows"))
+            {
+                Runtime.getRuntime().exec("cls");
+            }
+            else
+            {
+                Runtime.getRuntime().exec("clear");
+            }
+        }
+        catch (final Exception e)
+        {
+            e.printStackTrace();
+            //  Handle any exceptions.
+        }
+    }
+
+    /**
+     * Write Message in console
+     * @param msg The message to be shown
+     * @param clear Shall the console be cleared before message shows
+     */
+    public final static void Msg(String msg, boolean clear)
+    {
+        if (clear){
+            Console.Clear();
+        }
+        System.out.println(msg);
+    }
+
+    private static Random ran = new Random();
+
+    /**
+     * Get random int
+     * @param min Minimum int
+     * @param max Maximum int
+     * @return returns a int between min and max
+     */
+    public final static int RandomInt(int min, int max){
+        return min + ran.nextInt(Math.abs(max-min));
+    }
+
+    /**
+     * Read Console line
+     * @return a string with user response.
+     */
+    public static final String readLine()
+    {
+        try{
+            java.io.Console console = System.console();
+            if (console != null)
+            {
+                return console.readLine();
+            }
+            else
+            {
+                return new BufferedReader(new InputStreamReader(System.in)).readLine();
+            }
+        }catch(Exception ex){
+            // if any error occurs
+            ex.printStackTrace();
+        }
+        return  "";
+    }
+}
--- src/com/company/MainGameLoop.java
+++ src/com/company/MainGameLoop.java
@@ -1,8 +1,173 @@
 package com.company;
 
+import com.company.MonsterTypes.Mage;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 public class MainGameLoop {
 
-    public static void main(String[] args) {
+    private static Map GameMap;
 
+    /**
+     * Main Game construcktor.
+     * Loading / reloading a new game.
+     * @param args Non use.
+     */
+    public static void main(String[] args) {
+        GameMap = new Map();
+
+        Interact(Dialog.Wellcome);
+
+        //The game loop
+        while (true){
+            TurnLoop();
+        }
+    }
+
+    enum MoveDir{
+        Up,Down,Left,Right;
+        public static MoveDir get(int i){
+            return values()[i];
+        }
+    }
+    /**
+     * Updates each character in the game by turn
+     */
+    private static void TurnLoop(){
+        for(Character charector: GameMap.Characters){
+            Point ChaPos = GameMap.WhereIs(charector);
+            //region Bot
+            if (charector instanceof Monster) {
+                //region Bot Move
+                boolean move = true;
+                ArrayList<MoveDir> dir = new ArrayList<MoveDir>();
+                while (move){
+                    MoveDir def;
+                    do {
+                        def = MoveDir.get(Console.RandomInt(0, 4));
+                    } while (dir.contains(def));
+                    dir.add(def);
+
+                    switch (def){
+                        case Up:
+                            if (GameMap.fetchAt(new Point(ChaPos.x,ChaPos.y-1)) == 0){
+                                GameMap.Map[ChaPos.x][ChaPos.y] = 0;
+                                GameMap.Map[ChaPos.x][ChaPos.y-1] = charector;
+                                move = false;
+                            }
+                            break;
+                        case Down:
+                            if (GameMap.fetchAt(new Point(ChaPos.x,ChaPos.y+1)) == 0){
+                                GameMap.Map[ChaPos.x][ChaPos.y] = 0;
+                                GameMap.Map[ChaPos.x][ChaPos.y+1] = charector;
+                                move = false;
+                            }
+                            break;
+                        case Left:
+                            if (GameMap.fetchAt(new Point(ChaPos.x-1,ChaPos.y)) == 0){
+                                GameMap.Map[ChaPos.x][ChaPos.y] = 0;
+                                GameMap.Map[ChaPos.x-1][ChaPos.y] = charector;
+                                move = false;
+                            }
+                            break;
+                        case Right:
+                            if (GameMap.fetchAt(new Pos(ChaPos.x+1,ChaPos.y)) == 0){
+                                GameMap.Map[ChaPos.x][ChaPos.y] = 0;
+                                GameMap.Map[ChaPos.x+1][ChaPos.y] = charector;
+                                move = false;
+                            }
+                            break;
+                    }
+                }
+                //endregion
+            }
+            //endregion
+            else if (charector instanceof Player){
+                boolean combat = false;
+                do{
+                    Object respons = Interact(Dialog.Move);
+                    if (respons instanceof MoveDir){
+                        Point newPos;
+                        switch ((MoveDir)respons){
+                            case Up:
+                                newPos = new Point(ChaPos.x,ChaPos.y-1);
+                                break;
+                            case Down:
+                                newPos = new Point(ChaPos.x,ChaPos.y+1);
+                                break;
+                            case Left:
+                                newPos = new Point(ChaPos.x-1,ChaPos.y);
+                                break;
+                            case Right:
+                                newPos = new Point(ChaPos.x+1,ChaPos.y);
+                                break;
+                        }
+                        Object checkPos = GameMap.fetchAt(newPos);
+                        if (checkPos instanceof Integer){
+                            if ((Integer)checkPos == 0){
+                                GameMap.Map[ChaPos.x][ChaPos.y] = 0;
+                                GameMap.Map[ChaPos.x+1][ChaPos.y] = charector;
+                                break;
+                            }
+                            else if ((Integer)checkPos == 1 ||(Integer)checkPos == -1){
+                                Console.Msg("You have ran into a wall.",true);
+                            }
+                        }
+                        else if (checkPos instanceof Monster){
+
+                        }
+                    }
+                }while(true);
+            }
+            //else if (Win){
+                //type hura....
+            //}
+        }
+    }
+    private boolean CombatLoop(){
+        return false;
+    }
+
+
+    enum Dialog {
+        Wellcome,Action,Move;
+    }
+
+    /**
+     * Interacts with user
+     * @param display Type of dialog
+     * @return Action required from user
+     */
+    private static Object Interact(Dialog display){
+        switch(display){
+            case Action:
+            default:
+
+                break;
+            case Move:
+                while(true){
+                    Console.Msg("Where do you want to go.",true);
+                    String Response = Console.readLine().toLowerCase();
+                    if (Response.equals("up") || Response.equals("u")){
+                        return MoveDir.Up;
+                    }
+                    else if (Response.equals("down")|| Response.equals("d")){
+                        return MoveDir.Down;
+                    }
+                    else if (Response.equals("left") || Response.equals("l")){
+                        return MoveDir.Left;
+                    }
+                    else if (Response.equals("right")|| Response.equals('r')){
+                        return MoveDir.Right;
+                    }
+                }
+            case Wellcome:
+                Console.Msg("Hello and welcome to the Java Rpg,",true);
+                Console.Msg("also known as, The Tower Of Doom.",false);
+                Console.Msg("Created by Jesper Baunsgaard and Daniel Jensen",false);
+                break;
+        }
+        return "";
     }
 }
